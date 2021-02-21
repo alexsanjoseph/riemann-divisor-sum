5b57df3920e84595efa0d0aba567e7be1b794099
alexsanjoseph <alexsanjoseph@gmail.com>
2021-02-22 00:56:07 +0530

    Can't install numba

--- riemann/counterexample_search.py
+++ riemann/counterexample_search.py
@@ -3,12 +3,16 @@ import math
 from riemann.divisor import divisor_sum
 
 
+SEARCH_START = 5041
+
+
 def witness_value(n: int) -> float:
     denominator = n * math.log(math.log(n))
     return divisor_sum(n) / denominator
 
 
-def search(max_range: int, search_start:int = 5041) -> int:
+def search(max_range: int, search_start: int = SEARCH_START) -> int:
+    '''Search for a counterexample to the Riemann Hypothesis.'''
     for n in range(search_start, max_range + 1):
         if witness_value(n) > 1.782:
             return n
@@ -16,3 +20,7 @@ def search(max_range: int, search_start:int = 5041) -> int:
     raise ValueError(
         "No witnesses found. "
         "Are you sure trying to disprove RH is wise?")
+
+def best_witness(max_range: int, search_start: int = SEARCH_START) -> int:
+    witness_value = [divisor_sum(i) for i in range(1, max_range)]
+    return max(range(search_start, max_range), key=witness_value)
--- riemann/divisor.py
+++ riemann/divisor.py
@@ -1,7 +1,23 @@
+
 '''Compute the sum of divisors of a number.'''
+import math
+from numba import njit
 
+
+@njit
 def divisor_sum(n: int) -> int:
+    '''Compute the sum of divisors of a positive integer.'''
     if n <= 0:
-        raise ValueError("Non-positive numbers are not supported.")
+        return 0
 
-    return sum(i for i in range(1, n+1) if n%i == 0)
+    i = 1
+    limit = math.sqrt(n)
+    the_sum = 0
+    while i <= limit:
+        if n % i == 0:
+            the_sum += i
+            if i != limit:
+                the_sum += n // i
+        i += 1
+
+    return the_sum
